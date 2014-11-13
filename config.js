c6535ca8127d253e46faabcdfcf46cf1a3379b75
Simon Bates <simon@bitstructures.com>
2014-11-13 13:40:37 -0500

    Init

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+config.authorizationServerPort = 3000;
+config.resourceServerPort = 3001;
+config.clientPort = 3002;
+
+module.exports = config;
---
+++ gpii-oauth2-resource-server/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var morgan = require('morgan');
+var passport = require('passport');
+var BearerStrategy = require('passport-http-bearer').Strategy;
+var config = require('../config');
+
+function getUser () {
+    return { id: 1, name: 'Some User' };
+}
+
+// Used to check the accessToken on protected APIs
+passport.use(new BearerStrategy(
+    function (accessToken, done) {
+        // here we would check the accessToken and find the associated user
+        console.log('access_token=' + accessToken);
+        return done(null, getUser());
+    }
+));
+
+var app = express();
+app.use(morgan(':method :url', { immediate: true }));
+app.use(passport.initialize());
+
+app.get('/secured',
+    passport.authenticate('bearer', { session: false }),
+    function (req, res) {
+        res.send('hello world');
+    }
+);
+
+app.listen(config.resourceServerPort);
