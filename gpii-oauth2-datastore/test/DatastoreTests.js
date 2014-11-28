0bb22e76d9732e9f174a25a2743a767be20116b7
Simon Bates <simon@bitstructures.com>
2014-11-28 14:46:39 -0500

    Use full object paths

--- gpii-oauth2-datastore/test/DatastoreTests.js
+++ gpii-oauth2-datastore/test/DatastoreTests.js
@@ -8,7 +8,7 @@ You may obtain a copy of the License at
 https://github.com/gpii/universal/LICENSE.txt
 */
 
-var fluid = require("infusion");
+var fluid = fluid || require("infusion");
 var jqUnit = fluid.require("jqUnit");
 var datastore = require("../index.js");
 
@@ -16,34 +16,35 @@ var datastore = require("../index.js");
 
     "use strict";
 
-    var datastoreTests = fluid.registerNamespace ("gpii.tests.oauth2datastore");
-    var testdata = fluid.registerNamespace ("gpii.tests.oauth2datastore.testdata");
+    var gpii = fluid.registerNamespace("gpii");
+    fluid.registerNamespace("gpii.tests.oauth2.datastore");
+    fluid.registerNamespace("gpii.tests.oauth2.datastore.testdata");
 
-    testdata.authDecision1 = {
+    gpii.tests.oauth2.datastore.testdata.authDecision1 = {
         userId: 1,
         clientId: 2,
         redirectUri: "http://example.com/callback",
         accessToken: "access_token_1"
     };
 
-    datastoreTests.saveAuthDecision1 = function (data) {
-        return data.saveAuthDecision(testdata.authDecision1.userId,
-            testdata.authDecision1.clientId,
-            testdata.authDecision1.redirectUri,
-            testdata.authDecision1.accessToken);
+    gpii.tests.oauth2.datastore.saveAuthDecision1 = function (data) {
+        return data.saveAuthDecision(gpii.tests.oauth2.datastore.testdata.authDecision1.userId,
+            gpii.tests.oauth2.datastore.testdata.authDecision1.clientId,
+            gpii.tests.oauth2.datastore.testdata.authDecision1.redirectUri,
+            gpii.tests.oauth2.datastore.testdata.authDecision1.accessToken);
     };
 
-    datastoreTests.findAuthDecision1 = function (data) {
-        return data.findAuthDecision(testdata.authDecision1.userId,
-            testdata.authDecision1.clientId,
-            testdata.authDecision1.redirectUri);
+    gpii.tests.oauth2.datastore.findAuthDecision1 = function (data) {
+        return data.findAuthDecision(gpii.tests.oauth2.datastore.testdata.authDecision1.userId,
+            gpii.tests.oauth2.datastore.testdata.authDecision1.clientId,
+            gpii.tests.oauth2.datastore.testdata.authDecision1.redirectUri);
     };
 
-    datastoreTests.verifyAuthDecision1 = function (authDecision) {
-        jqUnit.assertEquals("userId", testdata.authDecision1.userId, authDecision.userId);
-        jqUnit.assertEquals("clientId", testdata.authDecision1.clientId, authDecision.clientId);
-        jqUnit.assertEquals("redirectUri", testdata.authDecision1.redirectUri, authDecision.redirectUri);
-        jqUnit.assertEquals("accessToken", testdata.authDecision1.accessToken, authDecision.accessToken);
+    gpii.tests.oauth2.datastore.verifyAuthDecision1 = function (authDecision) {
+        jqUnit.assertEquals("userId", gpii.tests.oauth2.datastore.testdata.authDecision1.userId, authDecision.userId);
+        jqUnit.assertEquals("clientId", gpii.tests.oauth2.datastore.testdata.authDecision1.clientId, authDecision.clientId);
+        jqUnit.assertEquals("redirectUri", gpii.tests.oauth2.datastore.testdata.authDecision1.redirectUri, authDecision.redirectUri);
+        jqUnit.assertEquals("accessToken", gpii.tests.oauth2.datastore.testdata.authDecision1.accessToken, authDecision.accessToken);
         jqUnit.assertFalse("not revoked", authDecision.revoked);
     };
 
@@ -63,11 +64,11 @@ var datastore = require("../index.js");
 
     jqUnit.test("Save an Authorization Decision and retrieve it", function () {
         var data = datastore.createDatastore();
-        var authDecision1 = datastoreTests.saveAuthDecision1(data);
-        datastoreTests.verifyAuthDecision1(authDecision1);
+        var authDecision1 = gpii.tests.oauth2.datastore.saveAuthDecision1(data);
+        gpii.tests.oauth2.datastore.verifyAuthDecision1(authDecision1);
         jqUnit.assertValue("Id has been assigned", authDecision1.id);
-        var retrieved = datastoreTests.findAuthDecision1(data);
-        datastoreTests.verifyAuthDecision1(retrieved);
+        var retrieved = gpii.tests.oauth2.datastore.findAuthDecision1(data);
+        gpii.tests.oauth2.datastore.verifyAuthDecision1(retrieved);
     });
 
 })();
