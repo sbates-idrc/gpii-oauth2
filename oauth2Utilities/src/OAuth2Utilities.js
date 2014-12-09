bb21d074f5e3f0942a2b24867bd4b2a5f3e5a28f
Simon Bates <simon@bitstructures.com>
2014-12-09 15:04:24 -0500

    GPII-1038: Remove Passport from the Resource Server
    And implement the access_token header parsing ourselves

--- oauth2Utilities/src/OAuth2Utilities.js
+++ oauth2Utilities/src/OAuth2Utilities.js
@@ -5,6 +5,16 @@ var fluid = fluid || require("infusion");
 var gpii = fluid.registerNamespace("gpii");
 fluid.registerNamespace("gpii.oauth2");
 
+gpii.oauth2.parseBearerAuthorizationHeader = function (req) {
+    if (req.headers && req.headers.authorization) {
+        var parts = req.headers.authorization.split(/\s+/);
+        if (parts.length === 2 && parts[0].toLowerCase() === "bearer") {
+            return parts[1];
+        }
+    }
+    return undefined;
+};
+
 gpii.oauth2.walkMiddleware = function (middleware, i, req, res, next) {
     // TODO best way to check if middleware is a single function?
     if (typeof middleware === "function") {
